a96bc58039c947bb5a51c138846e4525d8c3233c
Steven Leipold <steven_lpld@outlook.com>
2021-11-04 10:31:40 +0100

    next try

--- src/main/java/de/htwberlin/steven/tutorial/web/api/PersonRestController.java
+++ src/main/java/de/htwberlin/steven/tutorial/web/api/PersonRestController.java
@@ -1,2 +1,26 @@
-package de.htwberlin.steven.tutorial.web.api;public class PersonRestController {
+package de.htwberlin.steven.tutorial.web.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonRestController {
+
+    private List<Person> persons;
+
+    public PersonRestController(){
+        persons = new ArrayList<>();
+        persons.add(new Person(1, "Max", "Mustermann", false));
+        persons.add(new Person(2, "Maxima", "Maier", true));
+    }
+
+    @GetMapping(path = "/api/v1/persons")
+    //Alternativ ohne ResponseEntity und @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<List<Person>> fetchPersons() {
+        return ResponseEntity.ok(persons);
+    }
 }
